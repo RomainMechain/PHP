97de5a626693aac18a5213d8148a95f4be1ba4ab
RomainMechain <romain.mechain@etu.univ-orleans.fr>
2024-01-19 12:39:33 +0100

    feat/ avancement dans la recup des réponses.

--- Actions/form.php
+++ Actions/form.php
@@ -1,12 +1,17 @@
 <?php
+session_start(); // Démarre une nouvelle session ou reprend une session existante
 
-    echo "<h1> Questionnaire </h1>";
-    echo "<form action='./Actions/submit.php' method='post'>";
-    $factory = new Factory('./Data/quizz.json');
-    $lst_res = $factory->create();
-    foreach ($lst_res as $instance) {
-        echo $instance.PHP_EOL;
-    }
-    echo "<input type='submit' value='Envoyer'>";
-    echo "</form>";
+echo "<h1> Questionnaire </h1>";
+echo "<form action='./Actions/submit.php' method='post'>";
+$factory = new Factory('./Data/quizz.json');
+$lst_res = $factory->create();
+
+// Stocke $factory dans la session
+$_SESSION['lst_instance'] = serialize($lst_res);
+
+foreach ($lst_res as $instance) {
+    echo $instance.PHP_EOL;
+}
+echo "<input type='submit' value='Envoyer'>";
+echo "</form>";
 ?>
--- Actions/submit.php
+++ Actions/submit.php
@@ -1,7 +1,32 @@
 <?php
+session_start(); // Démarre une nouvelle session ou reprend une session existante
+require_once '../Classes/Autoloader.php';
+Autoloader::register();
+
+use Form\Type\Radio;
+use Form\Type\Input;
+use Form\GenericFormElement;
+use Form\InputRenderInterface;
+
+// Récupère $lst_instance de la session
+$lst_instance = unserialize($_SESSION['lst_instance']);
+
 echo "<h1> Resultat </h1>";
 foreach ($_POST as $key => $value) {
     echo "Le champ $key a la valeur $value.<br>";
+    $instanceRep = null;
+    foreach ($lst_instance as $instance) {
+        if ($instance->getName() == $key) {
+            $instanceRep = $instance;
+        }
+    }
+    if ($instanceRep != null) {
+        if ($instanceRep->getAnswer() == $value) {
+            echo "Bonne réponse !<br>";
+        } else {
+            echo "Mauvaise réponse !<br>";
+        }
+    }
 }
 
 ?>
