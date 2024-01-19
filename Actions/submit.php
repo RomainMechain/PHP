9dadab920f0dda42d92197de70c11e60248f2ffa
Debray Antoine <o22202147@info23-27.iut45.univ-orleans.fr>
2024-01-19 12:39:32 +0100

    avancement recuperation des reponses

--- Actions/submit.php
+++ Actions/submit.php
@@ -1,7 +1,29 @@
 <?php
+require_once '../Classes/Factory.php';
+require_once '../Classes/Autoloader.php';
+require_once '../Classes/Provider.php';
+Autoloader::register();
+
+$factory = new Factory('../Data/quizz.json');
+$lst_res = $factory->create();
+
+foreach ($lst_res as $instance) {
+    
+}
+
 echo "<h1> Resultat </h1>";
 foreach ($_POST as $key => $value) {
-    echo "Le champ $key a la valeur $value.<br>";
+    $instancecor=null;
+    foreach ($lst_res as $instance) {
+        if ($instance->getName() == $key) {
+            $instancecor = $instance;
+        }
+    }
+    if ($instancecor->getAnswer() == $value) {
+        echo "Bonne réponse pour la question ".$instancecor->getQuestion()."<br>";
+    } else {
+        echo "Mauvaise réponse pour la question ".$instancecor->getQuestion()."<br>";
+    }
 }
 
 ?>
